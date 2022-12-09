2d31ceceebfc715a2526772957879af9ffc22fa1
paulaanb <91721496+paulaanb@users.noreply.github.com>
2022-12-09 15:47:47 +0000

    saludos

--- paquete/hola/saludos.py
+++ paquete/hola/saludos.py
@@ -1,2 +1,9 @@
 def saludar():
-    print()
+    print("Hola, te estoy saludando desde la función saludar() " \
+          "del módulo saludos")
+
+class Saludo():
+    def __init__(self):
+        print("Hola, te estoy saludando desde el __init__"  \
+              "de la clase Saludo")
+
---
+++ saludos.py
@@ -0,0 +1,4 @@
+def saludar():
+    print("Hola, te estoy saludando desde la función saludar() " \
+            "del módulo saludos")
+
